0e1d87558497a9eaf66c21c4568e3ee95f0facd8
cjqw <2462457751@qq.com>
2017-03-04 21:31:29 +0800

    rewrite transport-function & edit readme

--- src/neural_network/lms.clj
+++
@@ -1,1 +0,0 @@
-(ns neural-network.lms)
--- src/neural_network/transport_function.clj
+++ src/neural_network/transport_function.clj
@@ -1,35 +1,7 @@
-(ns neural-network.transport-function
-  (:require [clojure.math.numeric-tower :as math]))
+(ns neural-network.transport-function)
 
-;; (expt 2 200)
-;; => 1606938044258990275541962092341162602522202993782792835301376
+(def hardlim #(if (< % 0) 0 1))
 
-(defn hardlim
-  [x]
-  (if (< x 0)
-    0
-    1))
-
-(defn hardlims
-  [x]
-  (if (< x 0)
-    -1
-    1))
+(def hardlims #(if (< % 0) -1 1))
 
 (def purelin (fn [x] x))
-
-(defn satlin
-  [x]
-  (cond
-    (< x 0) 0
-    (< 1 x) 1
-    :else x))
-
-(defn satlins
-  [x]
-  (cond
-    (< x -1) -1
-    (< 1 x) 1
-    :else x))
-
-(def logsig (fn [x] x))
